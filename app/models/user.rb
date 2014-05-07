4ead484c5a5b840342c99ba8a0f4cbe81279f845
Daniel Walin <dwalin1@gmail.com>
2014-05-07 14:11:47 -0700

    End Ch. 10 - user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
@@ -15,6 +16,11 @@ class User < ActiveRecord::Base
 
   def User.digest(token)
     Digest::SHA1.hexdigest(token.to_s)
+  end
+  
+  def feed
+    #preliminary
+    Micropost.where("user_id = ?", id)
   end
 
   private
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -47,7 +47,7 @@ describe "Authentication" do
             
       describe "when attempting to visit a protected page" do
 
-        describe "in the Users controller" do
+      describe "in the Users controller" do
         
           describe "visiting the edit page" do
             before { visit edit_user_path(user) }
@@ -70,6 +70,19 @@ describe "Authentication" do
             before { visit users_path }
             it { should have_title('Sign in') }
           end
+        end
+      end
+      
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
         end
       end
     end
